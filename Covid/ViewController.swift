f4dc9dca9fb97ff66c7f0a1564f1ce6de93274f0
SevukMuhammed <sevukmuhammed@gmail.com>
2020-04-22 00:48:31 +0300

    Initial Commit

---
+++ Covid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid
+//
+//  Created by user on 4/22/20.
+//  Copyright © 2020 Muhammed Sevuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
